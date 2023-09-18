2c366f6c0583c70bbbe54d3b2c8111893dbaf48f
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2023-09-18 09:16:45 +0200

    Cleanup, refactor

--- Phone/app/src/main/java/com/facilitation/phone/view/HomeFragment.kt
+++ Phone/app/src/main/java/com/facilitation/phone/view/HomeFragment.kt
@@ -30,7 +30,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.HelloVuzix.setOnClickListener {
-            homeViewModel.SayHelloToVuzix()
+            homeViewModel.sayHelloToVuzix()
         }
     }
     override fun onDestroyView() {
--- Phone/app/src/main/java/com/facilitation/phone/viewModel/HomeViewModel.kt
+++ Phone/app/src/main/java/com/facilitation/phone/viewModel/HomeViewModel.kt
@@ -4,23 +4,23 @@ import android.app.Application
 import android.content.Intent
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.vuzix.connectivity.sdk.Connectivity
+import com.vuzix.connectivity.sdk.Device
 
 class HomeViewModel(private val appContext: Application) : AndroidViewModel(appContext) {
 
     private val ACTION_GET = "com.facilitation.view.GET"
-    fun SayHelloToVuzix() {
-        getRemoteDeviceModelClicked()
-    }
-    private fun getRemoteDeviceModelClicked() {
-        val connectivity = Connectivity.get(appContext)
-        val device = connectivity.device
-        if (device != null) {
+    private val connectivity: Connectivity = Connectivity.get(appContext)
+    private val device: Device = connectivity.device
+
+    fun sayHelloToVuzix() {
+        if (device.name.equals("BPR Blade")) {
             val getIntent = Intent(this.ACTION_GET)
             connectivity.sendBroadcast(getIntent)
             Toast.makeText(appContext, "Hello to Vuzix sent", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(appContext, "Vuzix not found. Cannot send message.", Toast.LENGTH_SHORT).show()
         }
     }
 }
