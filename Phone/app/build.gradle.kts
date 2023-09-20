055a771738fef28d8523a8f55b51309a4b86caa1
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-09-20 08:45:57 +0200

    Adding Spotify SDK libs

--- Phone/app/build.gradle.kts
+++ Phone/app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("com.vuzix:connectivity-sdk:1.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation(files("libs/spotify-app-remote-release-0.8.0.aar"))
+    implementation(files("libs/spotify-auth-release-2.1.0.aar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Phone/build.gradle.kts
+++ Phone/build.gradle.kts
@@ -1,5 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    kotlin("jvm") version "1.9.10"
+}
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
