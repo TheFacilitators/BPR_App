0327cb31515caf969337c0ee4e54e50872abe4d6
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-11-29 07:39:23 +0100

    Model change for consistency with server side

--- Vuzix/app/src/main/java/com/facilitation/view/model/TrackDTO.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/model/TrackDTO.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 class TrackDTO(
-    @SerializedName("title") val title: String,
-    @SerializedName("artist") val artist: String,
-    @SerializedName("uri") val uri: String,
+    @SerializedName("title") var title: String,
+    @SerializedName("artist") var artist: String,
+    @SerializedName("uri") var uri: String,
     @SerializedName("favorite") var isFavorite: Boolean,
     // Marked transient so this variable doesn't need to be on the server side as well
     @Transient var isPlaying: Boolean
