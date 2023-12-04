516078714e29b599f3aa1504b8c5252f1574b372
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-04 08:46:57 +0100

    Code comments - ITapInput

--- Vuzix/app/src/main/java/com/facilitation/view/utility/ITapInput.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/utility/ITapInput.kt
@@ -2,6 +2,9 @@ package com.facilitation.view.utility
 
 import com.facilitation.view.utility.enums.TapToCommandEnum
 
+/** Interface to implement for methods to relay commands from Tap devices.*/
 interface ITapInput {
+	/** The method to overwrite for receiving TapToCommandEnum.
+	 * @param commandEnum the TapToCommandEnum to handle.*/
 	fun onInputReceived(commandEnum: TapToCommandEnum)
 }
