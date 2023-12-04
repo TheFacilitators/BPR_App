57c95705b271e9625ff22a1a421f19e6f45b8c4c
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-04 09:03:49 +0100

    Code comments - MyActivityLifecycleCallbacks

--- Vuzix/app/src/main/java/com/facilitation/view/utility/MyActivityLifecycleCallbacks.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/utility/MyActivityLifecycleCallbacks.kt
@@ -3,34 +3,48 @@ package com.facilitation.view.utility
 import android.app.Activity
 import android.app.Application.ActivityLifecycleCallbacks
 import android.os.Bundle
-import android.util.Log
 import java.io.Serializable
 
+/** A serializable customized class for handling the logic of activity lifecycle callbacks.
+ * @property currentActivity a nullable Activity  of the one currently being used.*/
 class MyActivityLifecycleCallbacks(currentActivity: Activity) : ActivityLifecycleCallbacks, Serializable {
     @Transient var currentActivity: Activity? = currentActivity
 
-    override fun onActivityCreated(p0: Activity, p1: Bundle?) {
-        currentActivity = p0
+    /** Called when the Activity calls super.onActivityCreated().
+     * Sets the currentActivity from the argument.
+     * @param activity the Activity from which this method was called.
+     * @param savedInstanceState a Bundle containing the state of the Activity.*/
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        currentActivity = activity
     }
 
-    override fun onActivityStarted(p0: Activity) {
-        currentActivity = p0
+    /** Called when the Activity calls super.onActivityStarted().
+     * Sets the currentActivity from the argument.
+     * @param activity the Activity from which this method was called.*/
+    override fun onActivityStarted(activity: Activity) {
+        currentActivity = activity
     }
 
-    override fun onActivityResumed(p0: Activity) {
-        currentActivity = p0
+    /** Called when the Activity calls super.onActivityResumed().
+     * Sets the currentActivity from the argument.
+     * @param activity the Activity from which this method was called.*/
+    override fun onActivityResumed(activity: Activity) {
+        currentActivity = activity
     }
 
-    override fun onActivityPaused(p0: Activity) {
+    override fun onActivityPaused(activity: Activity) {
     }
 
-    override fun onActivityStopped(p0: Activity) {
+    override fun onActivityStopped(activity: Activity) {
     }
 
-    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+    override fun onActivitySaveInstanceState(activity: Activity, instanceState: Bundle) {
     }
 
-    override fun onActivityDestroyed(p0: Activity) {
-        p0.application.unregisterActivityLifecycleCallbacks(this)
+    /** Called when the Activity calls super.onActivityDestroyed().
+     * Unregisters this activity lifecycle callbacks from the argument Activity 'application'.
+     * @param activity the Activity from which this method was called.*/
+    override fun onActivityDestroyed(activity: Activity) {
+        activity.application.unregisterActivityLifecycleCallbacks(this)
     }
 }
