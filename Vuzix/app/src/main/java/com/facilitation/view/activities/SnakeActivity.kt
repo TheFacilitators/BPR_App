9c0019bb7501ab77835f0b21c50dd66d0a336192
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-04 07:38:02 +0100

    Code comments - SnakeActivity

--- Vuzix/app/src/main/java/com/facilitation/view/activities/SnakeActivity.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/activities/SnakeActivity.kt
@@ -19,8 +19,8 @@ import com.facilitation.view.utility.MyActivityLifecycleCallbacks
 import com.facilitation.view.utility.Snake
 import com.facilitation.view.utility.enums.TapToCommandEnum
 
+/** Activity to handle the logic of Snake.*/
 class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
-
     private lateinit var snakeGame: Snake
     private lateinit var binding: ActivitySnakeBinding
     private val handler = Handler(Looper.getMainLooper())
@@ -30,6 +30,9 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
     private lateinit var inputMethodManager : InputMethodManager
     private lateinit var receiver: TapReceiver
 
+    /** Initializes activityLifecycleCallbacks, inputMethodManager, receiver and snakeGame.
+     * Sets on key listener for user input and a game over listener.
+     * Calls startGameLoop().*/
     override fun onCreate(savedInstanceState: Bundle?) {
         activityLifecycleCallbacks = intent.getSerializableExtra("callback") as MyActivityLifecycleCallbacks
         application.registerActivityLifecycleCallbacks(activityLifecycleCallbacks)
@@ -51,6 +54,9 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
         startGameLoop()
     }
 
+    /** If the gameLoopRunnable is instantiated then callbacks on it are removed.
+     * gameLoopRunnable is created with the function run() and the handler is called to post it
+     * with a delay of a thousand divided by the device frame rate.*/
     private fun startGameLoop() {
         if (gameLoopRunnable != null) {
             handler.removeCallbacks(gameLoopRunnable!!)
@@ -58,6 +64,12 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
         val frameRate = 10L
         val delayMillis = 1000L / frameRate
         gameLoopRunnable = object : Runnable {
+            /** If the snakeGame is active then update() is called with a posting delay of
+             * a thousand divided by the device frame rate.
+             * The canvas is locked and if it's not null then draw() is called on snakeGame &
+             * thereafter the canvas is unlocked and posted to the UI.
+             * Regardless of the state of the canvas, if the game is in active state then
+             * postDelayed() is called on the handler with the same posting delay as earlier.*/
             override fun run() {
                 if(snakeGame.gameActive){
                     snakeGame.update()
@@ -67,12 +79,14 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
                         binding.snakeView.holder.unlockCanvasAndPost(canvas)
                     }
                     handler.postDelayed(this, delayMillis)
-                    }
+                }
             }
         }
         handler.postDelayed(gameLoopRunnable as Runnable, delayMillis)
     }
 
+    /** If game over dialog is not already displayed: Creates & displays an un-cancelable dialog
+     * with the score earned in the game.*/
     private fun showGameOverDialog() {
         if (gameOverDialog != null && gameOverDialog!!.isShowing) {
             return
@@ -87,18 +101,23 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
         gameOverDialog?.show()
     }
 
+    /** Calls showGameOverDialog(), removes the callbacks on the handler for the gameLoopRunnable
+     * and sets the runnable to null.*/
     override fun onGameOver(score: Int) {
         showGameOverDialog()
         handler.removeCallbacks(gameLoopRunnable!!)
         gameLoopRunnable = null
     }
 
+    /** Dismisses the gameOverDialog, calls resetGame() on snakeGame and calls startGameLoop().*/
     fun restartGame(view: View) {
         gameOverDialog?.dismiss()
         snakeGame.resetGame()
         startGameLoop()
     }
 
+    /** Dismisses the gameOverDialog, calls finishAffinity() and creates & starts an intent for
+     * MainActivity.*/
     fun exitGame(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
@@ -106,6 +125,8 @@ class SnakeActivity : AppCompatActivity(), Snake.GameOverListener, ITapInput {
         finishAffinity()
     }
 
+    /** Delegating handling of the input from the Tap device to the inputMethodManager.
+     * @param commandEnum a TapToCommandEnum containing the specific command to execute.*/
     override fun onInputReceived(commandEnum: TapToCommandEnum) {
         inputMethodManager.dispatchKeyEventFromInputMethod(binding.root, KeyEvent(
             KeyEvent.ACTION_DOWN, commandEnum.keyCode())
