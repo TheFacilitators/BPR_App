9970ec032554448ca06fcba5d339d26d56bd9566
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-05 07:47:34 +0100

    Code comments - ViewApplication

--- Vuzix/app/src/main/java/com/facilitation/view/ViewApplication.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/ViewApplication.kt
@@ -6,11 +6,15 @@ import android.util.Log
 import com.facilitation.view.utility.BluetoothHandler
 import com.vuzix.hud.resources.DynamicThemeApplication
 
-
+/** The application class itself.
+ * @property bluetoothHandler custom handler class for handling Bluetooth communication.
+ * @property bluetoothAdapter adapter for the handler.
+ * @see BluetoothHandler*/
 class ViewApplication : DynamicThemeApplication() {
     var bluetoothHandler: BluetoothHandler? = null
     var bluetoothAdapter: BluetoothAdapter? = null
 
+    /** Calls super.onCreate() and attempts to call connectToBluetooth().*/
     override fun onCreate() {
         super.onCreate()
         try {
@@ -20,6 +24,9 @@ class ViewApplication : DynamicThemeApplication() {
         }
     }
 
+    /** Initializes bluetoothAdapter as the default adapter. If it is null the bluetoothHandler
+     * is initialized with this class as the context argument. Then initiateConnection() is
+     * called on bluetoothHandler.*/
     fun connectToBluetooth() {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter != null) {
@@ -28,10 +35,14 @@ class ViewApplication : DynamicThemeApplication() {
         }
     }
 
+    /** Getter for the resource ID of the default theme.
+     * @return an integer ID value from style folder.*/
     override fun getNormalThemeResId(): Int {
         return R.style.AppTheme
     }
 
+    /** Getter for the resource ID of the light theme.
+     * @return an integer ID value from style folder.*/
     override fun getLightThemeResId(): Int {
         return R.style.AppTheme_Light
     }
