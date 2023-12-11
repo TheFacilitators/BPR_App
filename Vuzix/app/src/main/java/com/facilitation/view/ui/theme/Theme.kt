d63546509808f8be5853cfef38b943ce0eb69ebb
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-11 07:35:31 +0100

    Merging Type & Theme

--- Vuzix/app/src/main/java/com/facilitation/view/ui/theme/Theme.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -14,6 +15,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import com.facilitation.view.R
 
@@ -27,6 +32,16 @@ private val LightColorScheme = lightColorScheme(
     primary = Color(R.color.Purple40),
     secondary = Color(R.color.PurpleGrey40),
     tertiary = Color(R.color.Pink40)
+)
+
+private val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
 )
 
 @Composable
