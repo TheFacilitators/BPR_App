489911f8e872381122b7617145d847fc63280896
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2023-12-11 07:23:40 +0100

    Merging colors.cml & Color.kt

--- Vuzix/app/src/main/java/com/facilitation/view/ui/theme/Theme.kt
+++ Vuzix/app/src/main/java/com/facilitation/view/ui/theme/Theme.kt
@@ -10,37 +10,28 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import com.facilitation.view.R
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color(R.color.Purple80),
+    secondary = Color(R.color.PurpleGrey80),
+    tertiary = Color(R.color.Pink80)
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Color(R.color.Purple40),
+    secondary = Color(R.color.PurpleGrey40),
+    tertiary = Color(R.color.Pink40)
 )
 
 @Composable
 fun ViewTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
